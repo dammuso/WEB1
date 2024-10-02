8ce8f712d50f97381b559679c0152f7d390f271e
dammuso <sodam0414@naver.com>
2024-10-02 19:38:39 +0900

    first version

---
+++ main.js
@@ -0,0 +1,92 @@
+
+
+
+function calculate(number1, number2) {
+
+//문자와 문자를 합한거잖아.
+  //console.log(number1+number2)
+  
+//합한 문자를 숫자로 만들어 
+//parseInt num1= 'number1';
+//parseInt num2= 'number2';
+
+let num1 = parseInt(number1);
+let num2 = parseInt(number2);
+
+//+일때
+
+
+//-일때
+
+
+//x일때
+
+
+//%일때
+
+
+console.log(num1+num2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // console.log(number1);
+  // console.log(number2);
+  // console.log(num+num2);
+  
+
+// console.log(parseInt(number1) + parseInt(number2));
+     //console.log(2+5);
+
+// const num1= parseInt(number1);
+     //const numerTwo = 2;
+
+
+// const num2= parseInt(number2);
+     //const numberFive = 5;
+
+
+// console.log(number1+number2);
+     //console.log(numerTwo + numberFive);
+
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+}
+
+
+function three(){
+     3*
+}
